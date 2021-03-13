863f502942478271e4164cba91a36570c63d53fb
phan thij hiep <phanhiepcit@gmail.com>
2021-03-13 11:28:25 +0700

    refactor code

--- app/src/main/java/com/example/notebook/interfaces/IOnClickItemNoteHome.kt
+++
@@ -1,7 +0,0 @@
-package com.example.notebook.interfaces
-
-import com.example.notebook.models.NoteBook
-
-interface IOnClickItemNoteHome {
-    fun onClickItemNote(note: NoteBook)
-}
--- app/src/main/java/com/example/notebook/models/NoteBook.kt
+++
@@ -1,7 +0,0 @@
-package com.example.notebook.models
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class NoteBook(val title:String, val content: String, val date: String) : Parcelable
